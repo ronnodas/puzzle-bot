525630cea48c8e1271a8f78736b12bfa4ec31f2f
Ronno Das <ronnodas@gmail.com>
2021-06-28 09:19:58 -0500

    added type hints

--- bot.py
+++ bot.py
@@ -3,11 +3,11 @@
 
 import asyncio
 import os
+from typing import Optional
 
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
@@ -28,17 +28,17 @@ help_command = commands.DefaultHelpCommand(no_category='Commands')
 bot = commands.Bot(commands.when_mentioned, description=description, intents=intents, help_command=help_command)
 
 
-def get_category_by_name(ctx, name):
+def get_category_by_name(ctx: discord.Member, name: str) -> discord.CategoryChannel:
     return discord.utils.get(ctx.guild.categories, name=name)
 
 
-def get_party_channel(ctx):
+def get_party_channel(ctx: discord.Member) -> discord.TextChannel:
     for channel in ctx.guild.text_channels:
         if channel.name.startswith("party-of"):
             return channel
 
 
-def get_party_count(ctx):
+def get_party_count(ctx: discord.Member) -> int:
     solved_category = get_category_by_name(ctx, "Solved")
     solved_number = len(solved_category.channels)
     for i in range(2, 10):
@@ -49,7 +49,7 @@ def get_party_count(ctx):
     return len(ctx.guild.members) - solved_number
 
 
-async def add_puzzle_text_channel(ctx, name):
+async def add_puzzle_text_channel(ctx: discord.Member, name: str) -> discord.TextChannel:
     puzzle_category = get_category_by_name(ctx, "Puzzles")
     channel = discord.utils.get(ctx.guild.text_channels, topic=name)
     if not channel:
@@ -58,7 +58,7 @@ async def add_puzzle_text_channel(ctx, name):
         return channel
 
 
-async def remove_voice_channel(ctx, name):
+async def remove_voice_channel(ctx: discord.Member, name: str) -> bool:
     channel = discord.utils.get(ctx.guild.voice_channels, name=name)
     if channel is None:
         return False
@@ -70,14 +70,17 @@ async def remove_voice_channel(ctx, name):
         return True
 
 
-async def add_voice_channel(ctx, name, check_if_exists=False):
-    if check_if_exists and discord.utils.get(ctx.guild.voice_channels, name=name):
-        return await ctx.send(f"Voice channel with name '{name}' already exists")
+async def add_voice_channel(ctx: discord.Member, name: str, check_if_exists: bool = False) -> discord.VoiceChannel:
+    if check_if_exists:
+        voice_channel = discord.utils.get(ctx.guild.voice_channels, name=name)
+        if voice_channel:
+            await ctx.send(f"Voice channel with name '{name}' already exists")
+            return voice_channel
     voice_category = get_category_by_name(ctx, "Puzzle Voice Channels")
     return await ctx.guild.create_voice_channel(name, category=voice_category, topic=name)
 
 
-async def toggle_puzzle_voice_channel(ctx, name):
+async def toggle_puzzle_voice_channel(ctx: discord.Member, name: str) -> bool:
     channel = discord.utils.get(ctx.guild.voice_channels, name=name)
     if not channel:
         await add_voice_channel(ctx, name)
@@ -86,7 +89,7 @@ async def toggle_puzzle_voice_channel(ctx, name):
         return await remove_voice_channel(ctx, name)
 
 
-async def update_party_size_passively(ctx):
+async def update_party_size_passively(ctx: discord.Member) -> int:
     party_channel = get_party_channel(ctx)
     n = get_party_count(ctx)
     if n >= 0:
@@ -97,7 +100,7 @@ async def update_party_size_passively(ctx):
     return n
 
 
-def add_spreadsheet(title):
+def add_spreadsheet(title: str) -> str:
     refresh_drive_token_if_expired()
     # TODO: also check solved?
     search_list = drive.ListFile({'q': f"mimeType = 'application/vnd.google-apps.spreadsheet' and title = '{title}' "
@@ -113,7 +116,7 @@ def add_spreadsheet(title):
     return spreadsheet['alternateLink']
 
 
-def move_spreadsheet_to_solved(title):
+def move_spreadsheet_to_solved(title: str) -> None:
     refresh_drive_token_if_expired()
     search_list = drive.ListFile({'q': f"mimeType = 'application/vnd.google-apps.spreadsheet' and title = '{title}' "
                                        f"and '{default_folder_id}' in parents and trashed = false"}).GetList()
@@ -122,14 +125,14 @@ def move_spreadsheet_to_solved(title):
         spreadsheet.Upload()
 
 
-def refresh_drive_token_if_expired():
+def refresh_drive_token_if_expired() -> None:
     if google_authentication.access_token_expired:
         google_authentication.Refresh()
         google_authentication.SaveCredentialsFile(SAVED_CREDENTIALS_FILE)
 
 
 @bot.event
-async def on_member_join(member):
+async def on_member_join(member: discord.Member) -> None:
     party_channel = get_party_channel(member)
     n = await update_party_size_passively(member)
     name = member.nick if member.nick is not None else member.name
@@ -138,7 +141,7 @@ async def on_member_join(member):
 
 
 @bot.event
-async def on_member_remove(member):
+async def on_member_remove(member: discord.Member) -> None:
     party_channel = get_party_channel(member)
     n = await update_party_size_passively(member)
     name = member.nick if member.nick is not None else member.name
@@ -147,7 +150,7 @@ async def on_member_remove(member):
 
 
 @bot.event
-async def on_ready():
+async def on_ready() -> None:
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
@@ -155,7 +158,7 @@ async def on_ready():
 
 
 @bot.command(aliases=['p'])
-async def puzzle(ctx, *multi_word_title):
+async def puzzle(ctx: discord.Member, *multi_word_title: str) -> Optional[discord.Message]:
     """Adds a puzzle
 
     Creates channels and spreadsheets associated to a new puzzle.
@@ -178,7 +181,7 @@ async def puzzle(ctx, *multi_word_title):
 
 @bot.command()
 @commands.has_permissions(administrator=True)
-async def remove(ctx, *multi_word_title):
+async def remove(ctx: discord.Member, *multi_word_title: str) -> Optional[discord.Message]:
     """Removes puzzle channels and spreadsheets
 
     Removes all channels and spreadsheets associated with the puzzle title. Only available with administrator
@@ -202,7 +205,7 @@ async def remove(ctx, *multi_word_title):
 
 
 @bot.command(aliases=['v'])
-async def voice(ctx):
+async def voice(ctx: discord.Member) -> None:
     """Toggles voice channel
 
     Toggles voice channel on or off for a given puzzle. Does not turn off if currently in use. Can only be used
@@ -219,7 +222,7 @@ async def voice(ctx):
 
 
 @bot.command(aliases=['s'])
-async def solve(ctx):
+async def solve(ctx: discord.Member) -> Optional[discord.Message]:
     """Marks puzzle as solved
 
     Moves the text channel and spreadsheet (?) associated to a puzzle to solved and removes the associated voice
@@ -245,7 +248,7 @@ async def solve(ctx):
 
 
 @bot.command(name="recount", aliases=['r'])
-async def update_party_size(ctx):
+async def update_party_size(ctx: discord.Member) -> None:
     """Updates party size
 
     Updates the party size count should it get out of sync for whatever reason. Actually renames the topmost
