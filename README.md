5e22e472dbc5947beb7f11c94d0151f2a8733cdd
Ronno Das <ronnodas@gmail.com>
2024-09-20 14:20:50 +0200

    2024 changes (w/ rounds optional)

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
 # README #
 
-This is a discord bot for helping with puzzle hunts, with Google Drive integration. Runs on python 3.6+
+This is a discord bot for helping with puzzle hunts, with Google Drive integration. Runs on python 3.8+ (probably, tested on 3.11)
 
 ## Initial setup ##
 
-1. Install dependencies from `requirements.txt`, eg by running `pip3 install -r requirements.txt`.
+1. Install dependencies from `requirements.txt`, eg by running `pip install -r requirements.txt`.
 2. Create a [Google Drive API project](https://console.cloud.google.com/cloud-resource-manager), obtain `client_secrets.json` and place it in the working directory
    (see [PyDrive quickstart](https://pythonhosted.org/PyDrive/quickstart.html#authentication)).
 3. Create an application in [Discord Developer Portal](https://discord.com/developers/applications) and obtain the application *token*, to include in `config.ini` on the next step.
 4. Copy `example-config.ini` to `config.ini` and fill in the fields with appropriate data:
-    * `[discord] > token`: the discord application token from the previous step
-    * `[discord] > guild id`: the id for the discord server that the bot will to run on
+    * `[discord] > token`: discord application token from the previous step
+    * `[discord] > guild id`: id for the discord server that the bot will to run on
       (open discord in browser and look at the url or use *Developer Mode*)
-    * `[Google Drive] > root folder`: the name of the folder to store the spreadsheets in a Google Drive
-      that the bot will have access to
+    * `[Google Drive] > root folder`: name of the folder to store the spreadsheets in a Google Drive
+      the bot will have access to
+    * `[general] > rounds <true|false>`: whether to organize by rounds (default: false)
 
 ## Running the bot ##
 
--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 # coding: utf-8
+
 from collections.abc import Callable, Coroutine, Iterator
 from configparser import ConfigParser
-from typing import Any, Optional, Union, cast
+from typing import Optional, Union, cast
+
 
 import disnake
 import pydrive2.auth  # type: ignore
 import pydrive2.drive  # type: ignore
-from disnake.ext import commands, tasks
+from disnake.ext import commands
 from disnake.interactions import ApplicationCommandInteraction as Interaction
 
 Channel = Union[disnake.TextChannel, disnake.Thread, disnake.VoiceChannel]
@@ -144,7 +146,7 @@ async def find_or_make_category(
 
 
 def title_converter(_, title: str) -> str:
-    return title.replace("'", "").replace('"', "")
+    return title.replace("'", "").replace('"', "").replace("#", "").strip()
 
 
 def category_has_prefix(
@@ -153,10 +155,19 @@ def category_has_prefix(
     return category is not None and category.name.lower().startswith(prefix.lower())
 
 
+def normalize_round_name(name):
+    return "".join(c.lower() for c in name if c.isalnum())
+
+
 title_param = commands.Param(
     name="title",
     description="Title of the puzzle including whatever characters",
     converter=title_converter,
+)
+
+round_param = commands.Param(
+    name="round",
+    description="Unambiguous prefix of the round to add the puzzle to",
 )
 
 
@@ -172,15 +183,26 @@ def get_admin_mention_or_empty(guild: disnake.Guild) -> str:
 
 class PuzzleBot:
     description = "A bot to help puzzle hunts"
-    puzzles_category_name = "🧩Puzzles"
     solved_category_name = "✅Solved"
-    voice_category_name = "🧩Puzzle Voice Channels"
+    general_category_name = "💭General"  # should probably be a configuration option
+    default_puzzle_category = "🧩Puzzles"
 
-    def __init__(self, token: str, guild_id: int, drive_root_folder: str) -> None:
+    def __init__(
+        self,
+        token: str,
+        guild_id: int,
+        drive_root_folder: str,
+        rounds: bool = False,
+        **_,
+    ) -> None:
         self.drive = PuzzleDrive(drive_root_folder)
 
         self.token = token
         self.guild_id = guild_id
+        self.use_rounds = rounds
+        self.known_rounds = {}
+        self.current_round = None if self.use_rounds else self.default_puzzle_category
+        self.voices_to_oppress = set()
         self.client = commands.InteractionBot(test_guilds=[guild_id])
 
         for event in self.events:
@@ -191,13 +213,21 @@ class PuzzleBot:
         self.client.run(self.token)
 
     @property
-    def events(self) -> Iterator[Callable[[Any], Coroutine]]:
+    def events(self) -> Iterator[Callable[..., Coroutine]]:
         yield self.on_ready
+        yield self.on_voice_state_update
 
     def register_commands(self) -> None:
         self.client.slash_command(name="puzzle", description="Add a puzzle")(
             self.add_puzzle
         )
+        if self.use_rounds:
+            self.client.slash_command(
+                name="puzzle-in-round", description="Add a puzzle to a round"
+            )(self.add_in_round)
+            self.client.slash_command(name="round", description="Add a round")(
+                self.round
+            )
         self.client.slash_command(
             description="Toggle voice channel, use in the puzzle's text channel"
         )(self.voice)
@@ -218,7 +248,6 @@ class PuzzleBot:
         #     default_member_permissions=disnake.Permissions(administrator=True),
         # )(self.channel_cleanup)
 
-    @tasks.loop(minutes=30.0)
     async def voice_cleanup(self, guild: Optional[disnake.Guild] = None) -> int:
         if guild is None:
             guild = self.client.get_guild(self.guild_id)
@@ -231,7 +260,6 @@ class PuzzleBot:
                 count += await self.remove_voice_channel(channel, None)
         return count
 
-    @voice_cleanup.before_loop
     async def before_voice_cleanup(self):
         await self.client.wait_until_ready()
 
@@ -243,26 +271,37 @@ class PuzzleBot:
     #     ]
 
     # async def channel_cleanup(self, interaction: Interaction) -> None:
+    #     await interaction.response.defer()
     #     guild = self.client.get_guild(self.guild_id)
     #     if guild is None:
     #         return
+    #     category = await find_or_make_category(guild, "archive 2023")
     #     channels = [
     #         channel
     #         for channel in guild.text_channels
     #         if self.get_puzzle_title(channel) is not None
+    #         and (channel_category := channel.category) is not None
+    #         and not channel_category.name.startswith("archive")
     #     ]
-    #     print(f"Removing {len(channels)} channels:")
+    #     # print(f"Removing {len(channels)} channels:")
+    #     # for channel in channels:
+    #     #     print(f"    {channel.name}")
+    #     # response = input("Confirm: (y/n)")
+    #     # if response.lower() != "y":
+    #     #     return
     #     for channel in channels:
-    #         print(f"    {channel.name}")
-    #     response = input("Confirm: (y/n)")
-    #     if response.lower() != "y":
-    #         return
-    #     for channel in channels:
-    #         channel.delete()
-    #     await self.manual_voice_cleanup()
+    #         await channel.edit(category=category)
+    #     await self.manual_voice_cleanup(interaction)
+    #     await add_reaction(interaction, THUMBS_UP)
 
     async def on_ready(self) -> None:
-        await self.create_categories(self.client.get_guild(self.guild_id))
+        guild = self.client.get_guild(self.guild_id)
+        if guild is not None:
+            await self.create_categories(guild)
+            self.known_rounds = self.parse_rounds(guild)
+            self.voices_to_oppress = await self.find_voices_to_oppress(guild)
+        else:
+            print(f"Could not access guild {self.guild_id}")
         print(f"Logged in as {self.client.user.name} (id #{self.client.user.id})")
 
     async def add_puzzle(
@@ -273,21 +312,48 @@ class PuzzleBot:
         guild = interaction.guild
         if guild is None:
             raise ValueError("Cannot access guild")
-        await interaction.send(f"Creating puzzle {puzzle_title}")
-        existing_channel = disnake.utils.get(guild.text_channels, topic=puzzle_title)
-        if existing_channel is not None:
-            await interaction.send(f"There's already a puzzle called {puzzle_title} 🤔")
+        if self.current_round is None:
+            await interaction.send("No round is currently active, use /puzzle-in-round")
             await add_reaction(interaction, THUMBS_DOWN)
-        link = self.drive.add_spreadsheet(puzzle_title)
-        channel = await self.add_puzzle_text_channel(guild, puzzle_title)
-        link_message = await channel.send(
-            f"I found a 📔spreadsheet for this puzzle at {link}"
+            return
+        await self.add_puzzle_channel_to_round(
+            interaction, puzzle_title, self.current_round, guild
         )
-        await link_message.pin()
-        await self.add_voice_channel(guild, puzzle_title)
-        await interaction.edit_original_message(
-            content=f'Created 🧩 "{puzzle_title}" at {channel.mention}'
+
+    async def add_in_round(
+        self,
+        interaction: Interaction,
+        puzzle_title: str = title_param,
+        round_name: str = round_param,
+    ) -> None:
+        guild = interaction.guild
+        if guild is None:
+            raise ValueError("Cannot access guild")
+        rounds = self.match_round(round_name)
+        if isinstance(rounds, list):
+            if not rounds:
+                error = f"Can't find a round matching {round_name}, if you meant to create a new round use /round"
+            else:
+                error = f"Found more than one round matching {round_name}: {'; '.join(rounds)}"
+            await interaction.send(error)
+            await add_reaction(interaction, THUMBS_DOWN)
+            return
+        round_name = rounds
+        return await self.add_puzzle_channel_to_round(
+            interaction, puzzle_title, round_name, guild
         )
+
+    async def round(
+        self, interaction: Interaction, round_name: str = round_param
+    ) -> None:
+        guild = interaction.guild
+        if guild is None:
+            raise ValueError("Cannot access guild")
+        await interaction.send(f"Creating round {round_name}")
+        await guild.create_category(name=round_name)
+        self.known_rounds[normalize_round_name(round_name)] = round_name
+        if self.use_rounds:
+            await self.set_round(round_name)
         await add_reaction(interaction, THUMBS_UP)
 
     async def solve(self, interaction: Interaction) -> Optional[str]:
@@ -314,7 +380,8 @@ class PuzzleBot:
             return await add_reaction(interaction, THUMBS_DOWN)
         await text_channel.edit(category=solved_category)
         self.drive.move_spreadsheet_to_solved(puzzle_title)
-        await self.find_and_remove_voice_channel(interaction, puzzle_title)
+        if not await self.find_and_remove_voice_channel(interaction, puzzle_title):
+            self.voices_to_oppress.add(puzzle_title)
         await add_reaction(interaction, THUMBS_UP)
         return puzzle_title
 
@@ -356,7 +423,9 @@ class PuzzleBot:
                 else THUMBS_DOWN
             )
         else:
-            await self.add_voice_channel(guild, puzzle_title)
+            await guild.create_voice_channel(
+                name=puzzle_title, category=text_channel.category
+            )
             reaction = THUMBS_UP
         await add_reaction(interaction, reaction)
 
@@ -366,6 +435,39 @@ class PuzzleBot:
         count = await self.voice_cleanup(guild)
         if count != 0:
             await interaction.edit_original_message(f"Removed {count} channel(s)")
+
+    async def on_voice_state_update(
+        self,
+        member: disnake.Member,
+        before: disnake.VoiceState,
+        after: disnake.VoiceState,
+    ) -> None:
+        channel = before.channel
+        if channel is None or after.channel == channel:
+            return
+        if not channel.members and (name := channel.name) in self.voices_to_oppress:
+            await channel.delete()
+            self.voices_to_oppress.remove(name)
+
+    async def find_voices_to_oppress(self, guild: disnake.Guild) -> set[str]:
+        return {
+            puzzle_title
+            for category in guild.categories
+            if category.name.startswith(self.solved_category_name)
+            for channel in category.text_channels
+            if (puzzle_title := self.get_puzzle_title(channel)) is not None
+            if not await self.find_and_remove_voice_channel(guild, puzzle_title)
+        }
+
+    async def create_categories(self, guild: disnake.Guild) -> None:
+        to_create = (
+            [self.solved_category_name]
+            if self.use_rounds
+            else [self.solved_category_name, self.default_puzzle_category]
+        )
+        for category in to_create:
+            if disnake.utils.get(guild.categories, name=category) is None:
+                await guild.create_category(category)
 
     def get_puzzle_title(
         self, channel: Channel, unsolved: bool = False
@@ -377,12 +479,75 @@ class PuzzleBot:
             return None
         return channel.topic.strip()
 
-    @classmethod
-    async def add_voice_channel(
-        cls, guild: disnake.Guild, name: str
-    ) -> disnake.VoiceChannel:
-        voice_category = await find_or_make_category(guild, cls.voice_category_name)
-        return await guild.create_voice_channel(name=name, category=voice_category)
+    def parse_rounds(self, guild: disnake.Guild) -> dict[str, str]:
+        rounds = {}
+        for category in guild.categories:
+            if category_has_prefix(category, self.solved_category_name):
+                continue
+            if category_has_prefix(category, "archive"):
+                continue
+            category = category.name
+            if category == self.general_category_name:
+                continue
+            normalized = normalize_round_name(category)
+            rounds[normalized] = category
+        return rounds
+
+    def match_round(self, round_name: str) -> str | list[str]:
+        round_name = normalize_round_name(round_name)
+        matches = [
+            name
+            for normalized, name in self.known_rounds.items()
+            if normalized.startswith(round_name)
+        ]
+        return matches[0] if len(matches) == 1 else matches
+
+    async def set_round(self, round_name: str) -> None:
+        self.current_round = round_name
+        activity = disnake.Activity(name=round_name)
+        await self.client.change_presence(activity=activity)
+
+    async def add_puzzle_channel_to_round(
+        self,
+        interaction: Interaction,
+        puzzle_title: str,
+        round_name: str,
+        guild: disnake.Guild,
+    ) -> None:
+        category = disnake.utils.get(guild.categories, name=round_name)
+        if category is None:
+            await interaction.send(
+                f"Something went wrong; maybe the category {round_name} was deleted or created manually?"
+            )
+            await add_reaction(interaction, THUMBS_DOWN)
+            self.known_rounds = self.parse_rounds(guild)
+            return
+        if self.use_rounds:
+            await interaction.send(
+                f"Creating puzzle {puzzle_title} in round {round_name}"
+            )
+        else:
+            await interaction.send(f"Creating puzzle {puzzle_title}")
+        existing_channel = disnake.utils.get(guild.text_channels, topic=puzzle_title)
+        if existing_channel is not None:
+            await interaction.send(
+                f"There's already a puzzle called {puzzle_title} at {existing_channel.mention}"
+            )
+            await add_reaction(interaction, THUMBS_DOWN)
+        link = self.drive.add_spreadsheet(puzzle_title)
+        channel = await self.add_puzzle_text_channel(
+            guild, puzzle_title, category=category
+        )
+        link_message = await channel.send(
+            f"I found a 📔spreadsheet for this puzzle at {link}"
+        )
+        await link_message.pin()
+        await guild.create_voice_channel(name=puzzle_title, category=category)
+        if self.use_rounds:
+            await self.set_round(round_name)
+        await interaction.edit_original_message(
+            content=f'Created 🧩 "{puzzle_title}" at {channel.mention}'
+        )
 
     @classmethod
     def run_from_config(cls) -> None:
@@ -392,28 +557,31 @@ class PuzzleBot:
             token=config["discord"]["token"],
             drive_root_folder=config["Google drive"]["root folder"],
             guild_id=int(config["discord"]["guild id"]),
+            rounds=config["general"].getboolean("rounds", fallback=False),
+            other_configs=config,
         )
         bot.start()
 
     @classmethod
     async def add_puzzle_text_channel(
-        cls, guild: disnake.Guild, puzzle_title: str
+        cls, guild: disnake.Guild, puzzle_title: str, category: disnake.CategoryChannel
     ) -> disnake.TextChannel:
-        puzzle_category_id = await cls.puzzle_category(guild)
         return await guild.create_text_channel(
-            name=puzzle_title, topic=puzzle_title, category=puzzle_category_id
+            name=puzzle_title, topic=puzzle_title, category=category
         )
 
     @classmethod
     async def find_and_remove_voice_channel(
-        cls, interaction: Interaction, name: str
-    ) -> None:
-        guild = interaction.guild
+        cls, context: Interaction | disnake.Guild, name: str
+    ) -> bool:
+        interaction = context if isinstance(context, Interaction) else None
+        guild = context.guild if isinstance(context, Interaction) else context
         if guild is None:
             raise ValueError("Could not access message guild")
         channel = disnake.utils.get(guild.voice_channels, name=name)
         if channel is not None:
-            await cls.remove_voice_channel(channel, interaction)
+            return await cls.remove_voice_channel(channel, interaction)
+        return True
 
     @classmethod
     async def remove_voice_channel(
@@ -426,20 +594,6 @@ class PuzzleBot:
         else:
             await voice_channel.delete()
             return True
-
-    @classmethod
-    async def puzzle_category(cls, guild: disnake.Guild) -> disnake.CategoryChannel:
-        return await find_or_make_category(guild, cls.puzzles_category_name)
-
-    @classmethod
-    async def create_categories(cls, guild: disnake.Guild) -> None:
-        for category in (
-            cls.puzzles_category_name,
-            cls.voice_category_name,
-            cls.solved_category_name,
-        ):
-            if disnake.utils.get(guild.categories, name=category) is None:
-                await guild.create_category(category)
 
 
 if __name__ == "__main__":
